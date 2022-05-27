054e7eb00e99fd9582b3fda70114b034dad8ced6
Andreu Salles <66407931+ndrslls@users.noreply.github.com>
2022-05-27 11:12:55 +0200

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container-large">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Work</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <div className="jumbo">Andreu Salles</div>
+        <div className="subtitle">
+          Hi, I am a <span>Product Designer</span> and{" "}
+          <span>Creative Developer</span>. I design and build products that are
+          meaningful, easy, and delightful to use.
+        </div>
+      </main>
     </div>
   );
 }
