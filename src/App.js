545a07f5fffb524b9138ea5f2b9c4c4ab872ac47
Andreu Salles <66407931+ndrslls@users.noreply.github.com>
2022-05-28 15:11:21 +0200

    adding description

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ import { Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="container-large">
-      <header className="App-header">
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>Work</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
-      </header>
       <main>
         <div className="jumbo">Andreu Salles</div>
-        <div className="subtitle">
-          Hi, I am a <span>Product Designer</span> and{" "}
-          <span>Creative Developer</span>. I design and build products that are
-          meaningful, easy, and delightful to use.
+        <div className="content-wrapper">
+          <div>
+            Hi, I am a <span>Frontend Developer</span> and{" "}
+            <span>Product Designer</span>. I design and build delightful
+            products in <span>React</span> and <span>Typescript</span>. My
+            portfolio is currently under construction. In the meanwhile, get in
+            touch and check{" "}
+            <a href="https://restyoureyes.app" target="_blank">
+              Rest Your Eyes{" "}
+            </a>
+            , an iOS App developed in <span>Swift</span> and{" "}
+            <span>SwiftUI</span>.
+          </div>
+          <div className="buttons-wrapper">
+            <a href="https://www.linkedin.com/in/andreusalles/" target="_blank">
+              <button className="cta">Get in touch!</button>
+            </a>
+          </div>
         </div>
       </main>
     </div>
---
+++ src/Components/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Work</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
